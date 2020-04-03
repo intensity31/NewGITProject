4d957a2a55fbf93c49ee5009a6a64e54d901d51c
Gaurav Sharma <gauravsharma@outlook.in>
2020-04-03 09:16:10 +0530

    Adding Test1 and Test2.
    Signed-off-by: Gaurav Sharma <gauravsharma@outlook.in>

---
+++ GitProject/src/App/Test1.java
@@ -0,0 +1,10 @@
+package App;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println("Test1");
+
+	}
+
+}
---
+++ GitProject/src/App/Test2.java
@@ -0,0 +1,10 @@
+package App;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		System.out.println("Test2");
+
+	}
+
+}
