ff8e4ad80bb9a66f9aa2c1eb1ef2f0c756454a3e
Gaurav Sharma <gauravsharma@outlook.in>
2020-07-18 10:58:45 +0530

    Added Test4.java

--- GitProject/src/App/Test4.java
+++ GitProject/src/App/Test4.java
@@ -1,10 +1,9 @@
 package App;
 
-public class Test3 {
-
+public class Test4 {
+	
 	public static void main(String[] args) {
 		System.out.println("Test3");
-		System.out.println("Test3 Again");
-	}
 
+	}
 }
